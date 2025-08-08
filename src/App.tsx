73599eece0944b8613a051040bafb5c4ecbe2372
Ritu Sangha <ritusangha@yahoo.com>
2025-08-08 16:42:44 -0700

    updated package and App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// App.tsx
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello, World!</Text>
+    </View>
+  );
+};
+
+export default App;
