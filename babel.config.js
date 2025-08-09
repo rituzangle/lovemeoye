ed22caec3b9431627c9f610111467f06ee0b3b0f
Ritu Sangha <ritusangha@yahoo.com>
2025-08-09 00:25:18 -0700

    resolved dependency issues

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,16 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo','@react-native/babel-preset'],
+    presets: [
+      'babel-preset-expo',
+      '@react-native/babel-preset'
+    ],
     plugins: [
       'react-native-reanimated/plugin',
       '@babel/plugin-transform-optional-catch-binding',
-      '@babel/plugin-transform-class-properties'
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: true }]
     ]
-  }
   };
+};
