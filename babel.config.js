6756730ba99f9f7bb475633961afb1534323cdbb
Ritu Sangha <ritusangha@yahoo.com>
2025-08-09 00:37:42 -0700

    simplified babel.config

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      'babel-preset-expo',
-      '@react-native/babel-preset'
-    ],
-    plugins: [
-      'react-native-reanimated/plugin',
-      '@babel/plugin-transform-optional-catch-binding',
-      ['@babel/plugin-transform-class-properties', { loose: true }],
-      ['@babel/plugin-transform-private-methods', { loose: true }],
-      ['@babel/plugin-transform-private-property-in-object', { loose: true }]
-    ]
+    presets: ['babel-preset-expo'],
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
